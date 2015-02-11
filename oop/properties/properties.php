f5d75c6d84ed879eeed5ba2efc1e5b69958951db
tri <tri@local.com>
2015-02-11 10:53:28 +0000

    oop

--- oop/properties/properties.php
+++ oop/properties/properties.php
@@ -1,6 +1,7 @@
 <?php
 class SimpleClass
 {
+	const CONSTANT = 1;
    // invalid property declarations:
    public $var1 = 'hello ' . 'world';
    public $var2 = <<<EOD
@@ -9,14 +10,19 @@ EOD;
    public $var3 = 1+2;
    public $var4 = self::myStaticMethod();
    public $var5 = $myVar;
-
-   // valid property declarations:
+   public $var8 = CONSTANT;
+   public $var9 = $this->publicFunc();
+   // valid property declarations: number, string, NULL, array
    public $var6 = myConstant;
    public $var7 = array(true, false);
-
+   
    // This is allowed only in PHP 5.3.0 and later.
    public $var8 = <<<'EOD'
 hello world
 EOD;
+
+	public function publicFunc() {
+		return 'p';
+	}
 }
 ?>
